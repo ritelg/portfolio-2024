cd1f9b9aa7bb2762f46c34824036067a9d658b5c
ritelg <ritelg@yahoo.fr>
2024-02-18 09:19:58 +0100

    feat(homepage): Add route for get portfolio

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/layout/includes/footer";
 import Header from "@/components/layout/includes/header";
 import {getAuthSession} from "@/libs/auth";
+import {seed} from "@/utils/seed-db";
 
 
 export default async function Home() {
   // const session = await getAuthSession();
-
   return (
    <>
    <Header />
